adee0a8275f692819f64c5f1a616b768d19aeba6
JRAD <jawagner@gmail.com>
2020-07-22 16:13:40 -0700

    added settings file

---
+++ Settings.h
@@ -0,0 +1,20 @@
+// wifi 
+#define ssid ""
+#define password ""
+
+// node information
+String node = "aq-01";
+String location = "mbr";
+#define hostName "esp-aq-01"
+
+// update frequency
+#define POLL_FREQ_SECONDS 5
+#define REPORT_FREQ_SECONDS 5
+
+// influxDb
+#define influxDbUrl ""
+#define influxAuth ""
+
+#define NTP_OFFSET   0      // In seconds
+#define NTP_INTERVAL 60 * 1000    // In miliseconds
+#define NTP_ADDRESS  "north-america.pool.ntp.org"
